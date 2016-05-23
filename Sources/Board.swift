2229e47ce3dbef5496525aea742ce3647bb0a5a2
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-05-23 17:32:05 -0400

    Add initializer and populate() method to Board

--- Sources/Board.swift
+++ Sources/Board.swift
@@ -64,6 +64,42 @@ public struct Board {
     /// The board's spaces.
     private var _spaces: [[Space]]
 
+    /// Creates a chess board.
+    ///
+    /// - Parameter populate: If `true`, the board is populated. Default is `true`.
+    public init(populate: Bool = true) {
+        let range = 0...7
+        self._spaces = range.reduce([]) { spaces, x in
+            spaces + [
+                range.reduce([]) {
+                    $0 + [Space(file: File(column: x)!, rank: Rank(row: $1)!)]
+                }
+            ]
+        }
+        if populate {
+            self.populate()
+        }
+    }
+
+    /// Populates `self` with with all of the pieces at their proper locations.
+    public mutating func populate() {
+        self.clear()
+        for x in 0...7 {
+            _spaces[x][1].piece = .Pawn(.White)
+            _spaces[x][6].piece = .Pawn(.Black)
+        }
+        for (y, color) in [(0, Color.White), (7, Color.Black)] {
+            _spaces[0][y].piece = .Rook(color)
+            _spaces[1][y].piece = .Knight(color)
+            _spaces[2][y].piece = .Bishop(color)
+            _spaces[3][y].piece = .Queen(color)
+            _spaces[4][y].piece = .King(color)
+            _spaces[5][y].piece = .Bishop(color)
+            _spaces[6][y].piece = .Knight(color)
+            _spaces[7][y].piece = .Rook(color)
+        }
+    }
+
     /// Clears all the pieces from `self`.
     public mutating func clear() {
         let range = 0...7
