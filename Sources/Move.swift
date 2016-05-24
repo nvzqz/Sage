f8d10bfd869c260ba8ae44175973d3c9c9160cb9
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-05-23 21:42:41 -0400

    Create Move struct

---
+++ Sources/Move.swift
@@ -0,0 +1,43 @@
+//
+//  Move.swift
+//  Chess
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2016 Nikolai Vazquez
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+/// A chess move.
+public struct Move {
+
+    /// The move's start position.
+    public var start: (File, Rank)
+
+    /// The move's end position.
+    public var end: (File, Rank)
+
+    /// Create a move with a start and end.
+    public init(start: (File, Rank), end: (File, Rank)) {
+        self.start = start
+        self.end = end
+    }
+
+}
