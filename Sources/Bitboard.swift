bda1a5fb1b4fa4cbc1594904a588f0797a54e6b0
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-06-21 22:22:57 -0400

    Add isEmpty to Bitboard and rename bitCount

--- Sources/Bitboard.swift
+++ Sources/Bitboard.swift
@@ -44,11 +44,16 @@ public struct Bitboard: BitwiseOperationsType, RawRepresentable, Equatable, Hash
     }
 
     /// The number of bits set in `self`.
-    public var bitCount: Int {
+    public var count: Int {
         var n = rawValue
         n = n - ((n >> 1) & 0x5555555555555555)
         n = (n & 0x3333333333333333) + ((n >> 2) & 0x3333333333333333)
         return Int((((n + (n >> 4)) & 0xF0F0F0F0F0F0F0F) &* 0x101010101010101) >> 56)
+    }
+
+    /// `true` if `self` is empty.
+    public var isEmpty: Bool {
+        return self == 0
     }
 
     /// Convert from a raw value of `UInt64`.
