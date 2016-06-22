efe92ceb632aa550bc677dda89863a48a0dc009b
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-06-21 22:04:32 -0400

    Add bitCount to Bitboard

--- Sources/Bitboard.swift
+++ Sources/Bitboard.swift
@@ -43,6 +43,14 @@ public struct Bitboard: BitwiseOperationsType, RawRepresentable, Equatable, Hash
         return rawValue.hashValue
     }
 
+    /// The number of bits set in `self`.
+    public var bitCount: Int {
+        var n = rawValue
+        n = n - ((n >> 1) & 0x5555555555555555)
+        n = (n & 0x3333333333333333) + ((n >> 2) & 0x3333333333333333)
+        return Int((((n + (n >> 4)) & 0xF0F0F0F0F0F0F0F) &* 0x101010101010101) >> 56)
+    }
+
     /// Convert from a raw value of `UInt64`.
     public init(rawValue: UInt64) {
         self.rawValue = rawValue
