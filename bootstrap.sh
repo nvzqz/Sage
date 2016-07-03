2107ac618ede520517e5cb98fedd9738d6a4cb21
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-07-03 00:50:46 -0400

    Update bootstrap.sh to use swift 3.0 preview 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,40 +6,15 @@
 
 set -e
 
-# Variables
-SWIFT_SNAPSHOT="swift-2.2.1-RELEASE"
-SWIFT_SNAPSHOT_TAR="$SWIFT_SNAPSHOT-ubuntu14.04.tar.gz"
-SWIFT_SNAPSHOT_URL="https://swift.org/builds/swift-2.2.1-release/ubuntu1404/$SWIFT_SNAPSHOT/$SWIFT_SNAPSHOT_TAR"
-SWIFT_SNAPSHOT_DIR="swift"
-SWIFT_TAR_FILE="swift_snapshot.tar.gz"
+SWIFT_DIR="swift-3.0-PREVIEW-1-ubuntu14.04"
+SWIFT_TAR="$SWIFT_DIR.tar.gz"
+SWIFT_URL="https://swift.org/builds/swift-3.0-preview-1/ubuntu1404/swift-3.0-PREVIEW-1/$SWIFT_TAR"
 
-# Update package list
-echo "Updating package list..."
 apt-get update
-
-# Install packages
-echo "Installing packages..."
 apt-get install -y git clang libicu-dev
 
-# Download the snapshot
-echo "Downloading Swift snapshot from $SWIFT_SNAPSHOT_URL"
-wget -q -O $SWIFT_TAR_FILE $SWIFT_SNAPSHOT_URL
-
-# Extract the snapshot
-echo "Extracting '$SWIFT_TAR_FILE' to '$SWIFT_SNAPSHOT_DIR'..."
-mkdir -p $SWIFT_SNAPSHOT_DIR && tar xzf $SWIFT_TAR_FILE -C $SWIFT_SNAPSHOT_DIR --strip-components=1
-
-# Set directory permissions
-echo "Setting permissions for '$SWIFT_SNAPSHOT_DIR'"
-chown -R vagrant $SWIFT_SNAPSHOT_DIR
-
-# Export swift to $PATH
-echo "Adding '$SWIFT_SNAPSHOT_DIR' directory to \$PATH..."
-echo "export PATH=/home/vagrant/$SWIFT_SNAPSHOT_DIR/usr/bin:$PATH" >> .profile
-
-# Remove swift snapshot archive
-echo "Cleaning up..."
-rm $SWIFT_TAR_FILE
-
-# Finished
-echo "Successfully installed Swift!"
+curl -O $SWIFT_URL
+tar zxf $SWIFT_TAR
+echo "export PATH=/home/vagrant/$SWIFT_DIR/usr/bin:$PATH" >> .profile
+chown -R vagrant $SWIFT_DIR
+rm $SWIFT_TAR
