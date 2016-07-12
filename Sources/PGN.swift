0c1d30425be12c5b07cb3f85ff7f0235ed43aa1f
Nikolai Vazquez <nvazquez1297@gmail.com>
2016-07-12 15:09:40 -0400

    Add init(parse:) to PGN

--- Sources/PGN.swift
+++ Sources/PGN.swift
@@ -269,6 +269,64 @@ public struct PGN {
 
     }
 
+    #if swift(>=3)
+
+    /// An error thrown by `PGN.init(parse:)`.
+    public enum ParseError: ErrorProtocol {
+
+        /// Unexpected quote found in move text.
+        case unexpectedQuote(String)
+
+        /// Unexpected closing brace found outside of comment.
+        case unexpectedClosingBrace(String)
+
+        /// No closing brace for comment.
+        case noClosingBrace(String)
+
+        /// No closing quote for tag value.
+        case noClosingQuote(String)
+
+        /// No closing bracket for tag pair.
+        case noClosingBracket(String)
+
+        /// Wrong number of tokens for tag pair.
+        case tagPairTokenCount([String])
+
+        /// Incorrect count of parenthesis for recursive annotation variation.
+        case parenthesisCountForRAV(String)
+
+    }
+
+    #else
+
+    /// An error thrown by `PGN.init(parse:)`.
+    public enum ParseError: ErrorType {
+
+        /// Unexpected quote found in move text.
+        case UnexpectedQuote(String)
+
+        /// Unexpected closing brace found outside of comment.
+        case UnexpectedClosingBrace(String)
+
+        /// No closing brace for comment.
+        case NoClosingBrace(String)
+
+        /// No closing quote for tag value.
+        case NoClosingQuote(String)
+
+        /// No closing bracket for tag pair.
+        case NoClosingBracket(String)
+
+        /// Wrong number of tokens for tag pair.
+        case TagPairTokenCount([String])
+
+        /// Incorrect count of parenthesis for recursive annotation variation.
+        case ParenthesisCountForRAV(String)
+
+    }
+
+    #endif
+
     /// The tag pairs for `self`.
     public var tagPairs: [String: String]
 
@@ -277,12 +335,22 @@ public struct PGN {
 
     /// The game outcome.
     public var outcome: Game.Outcome? {
-        #if swift(>=3)
-            let resultTag = Tag.result
-        #else
-            let resultTag = Tag.Result
-        #endif
-        return self[resultTag].flatMap(Game.Outcome.init)
+        get {
+            #if swift(>=3)
+                let resultTag = Tag.result
+            #else
+                let resultTag = Tag.Result
+            #endif
+            return self[resultTag].flatMap(Game.Outcome.init)
+        }
+        set {
+            #if swift(>=3)
+                let resultTag = Tag.result
+            #else
+                let resultTag = Tag.Result
+            #endif
+            self[resultTag] = newValue?.description
+        }
     }
 
     /// Create PGN with `tagPairs` and `moves`.
@@ -299,6 +367,28 @@ public struct PGN {
         }
     }
 
+    /// Create PGN by parsing `string`.
+    ///
+    /// - throws: `ParseError` if an error occured while parsing.
+    public init(parse string: String) throws {
+        self.init()
+        if string.isEmpty { return }
+        for line in string._splitByNewlines() {
+            if line.characters.first == "[" {
+                let commentsStripped = try line._commentsStripped(strings: true)
+                let (tag, value) = try commentsStripped._tagPair()
+                tagPairs[tag] = value
+            } else if line.characters.first?.isDigit == true {
+                let commentsStripped = try line._commentsStripped(strings: false)
+                let (moves, outcome) = try commentsStripped._moves()
+                self.moves += moves
+                if let outcome = outcome {
+                    self.outcome = outcome
+                }
+            }
+        }
+    }
+
     /// Get or set the value for `tag`.
     public subscript(tag: Tag) -> String? {
         get {
@@ -307,6 +397,197 @@ public struct PGN {
         set {
             tagPairs[tag.rawValue] = newValue
         }
+    }
+
+}
+
+private extension Character {
+
+    static let newlines: Set<Character> = ["\u{000A}", "\u{000B}", "\u{000C}", "\u{000D}",
+                                           "\u{0085}", "\u{2028}", "\u{2029}"]
+
+    static let whitespaces: Set<Character> = ["\u{0020}", "\u{00A0}", "\u{1680}", "\u{180E}", "\u{2000}",
+                                              "\u{2001}", "\u{2002}", "\u{2003}", "\u{2004}", "\u{2005}",
+                                              "\u{2006}", "\u{2007}", "\u{2008}", "\u{2009}", "\u{200A}",
+                                              "\u{200B}", "\u{202F}", "\u{205F}", "\u{3000}", "\u{FEFF}"]
+
+    static let digits: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    var isDigit: Bool {
+        return Character.digits.contains(self)
+    }
+
+}
+
+private extension String {
+
+    var _lastIndex: Index {
+        #if swift(>=3)
+            return index(before: endIndex)
+        #else
+            return endIndex.predecessor()
+        #endif
+    }
+
+    @inline(__always)
+    func _split(by set: Set<Character>) -> [String] {
+        return characters.split(isSeparator: set.contains).map(String.init)
+    }
+
+    @inline(__always)
+    func _splitByNewlines() -> [String] {
+        return _split(by: Character.newlines)
+    }
+
+    @inline(__always)
+    func _splitByWhitespaces() -> [String] {
+        return _split(by: Character.whitespaces)
+    }
+
+    @inline(__always)
+    func _tagPair() throws -> (String, String) {
+        guard characters.last == "]" else {
+            #if swift(>=3)
+                throw PGN.ParseError.noClosingBracket(self)
+            #else
+                throw PGN.ParseError.NoClosingBracket(self)
+            #endif
+        }
+        #if swift(>=3)
+            let startIndex = index(after: self.startIndex)
+            let endIndex = index(before: self.endIndex)
+        #else
+            let startIndex = self.startIndex.successor()
+            let endIndex = self.endIndex.predecessor()
+        #endif
+        let tokens = self[startIndex ..< endIndex]._split(by: ["\""])
+        guard tokens.count == 2 else {
+            #if swift(>=3)
+                throw PGN.ParseError.tagPairTokenCount(tokens)
+            #else
+                throw PGN.ParseError.TagPairTokenCount(tokens)
+            #endif
+        }
+        let tagParts = tokens[0]._splitByWhitespaces()
+        guard tagParts.count == 1 else {
+            #if swift(>=3)
+                throw PGN.ParseError.tagPairTokenCount(tagParts)
+            #else
+                throw PGN.ParseError.TagPairTokenCount(tagParts)
+            #endif
+        }
+        return (tagParts[0], tokens[1])
+    }
+
+    @inline(__always)
+    func _moves() throws -> (moves: [String], outcome: Game.Outcome?) {
+        var stripped = ""
+        var ravDepth = 0
+        var startIndex = self.startIndex
+        let lastIndex = _lastIndex
+        for (index, character) in zip(characters.indices, characters) {
+            if character == "(" {
+                if ravDepth == 0 {
+                    stripped += self[startIndex ..< index]
+                }
+                ravDepth += 1
+            } else if character == ")" {
+                ravDepth -= 1
+                if ravDepth == 0 {
+                    #if swift(>=3)
+                        startIndex = self.index(after: index)
+                    #else
+                        startIndex = index.successor()
+                    #endif
+                }
+            } else if index == lastIndex && ravDepth == 0 {
+                stripped += self[startIndex ... index]
+            }
+        }
+        guard ravDepth == 0 else {
+            #if swift(>=3)
+                throw PGN.ParseError.parenthesisCountForRAV(self)
+            #else
+                throw PGN.ParseError.ParenthesisCountForRAV(self)
+            #endif
+        }
+        let tokens = stripped._split(by: [" ", "."])
+        let moves = tokens.filter({ $0.characters.first?.isDigit == false })
+        let outcome = tokens.last.flatMap(Game.Outcome.init)
+        return (moves, outcome)
+    }
+
+    @inline(__always)
+    func _commentsStripped(strings consideringStrings: Bool) throws -> String {
+        var stripped = ""
+        var startIndex = self.startIndex
+        let lastIndex = _lastIndex
+        var afterEscape = false
+        var inString = false
+        var inComment = false
+        for (index, character) in zip(characters.indices, characters) {
+            if character == "\\" {
+                afterEscape = true
+                continue
+            }
+            if character == "\"" {
+                if !inComment {
+                    guard consideringStrings else {
+                        #if swift(>=3)
+                            throw PGN.ParseError.unexpectedQuote(self)
+                        #else
+                            throw PGN.ParseError.UnexpectedQuote(self)
+                        #endif
+                    }
+                    if !inString {
+                        inString = true
+                    } else if !afterEscape {
+                        inString = false
+                    }
+                }
+            } else if !inString {
+                if character == ";" && !inComment {
+                    stripped += self[startIndex ..< index]
+                    break
+                } else if character == "{" && !inComment {
+                    inComment = true
+                    stripped += self[startIndex ..< index]
+                } else if character == "}" {
+                    guard inComment else {
+                        #if swift(>=3)
+                            throw PGN.ParseError.unexpectedClosingBrace(self)
+                        #else
+                            throw PGN.ParseError.UnexpectedClosingBrace(self)
+                        #endif
+                    }
+                    inComment = false
+                    #if swift(>=3)
+                        startIndex = self.index(after: index)
+                    #else
+                        startIndex = index.successor()
+                    #endif
+                }
+            }
+            if index == lastIndex && !inComment {
+                stripped += self[startIndex ... index]
+            }
+            afterEscape = false
+        }
+        guard !inString else {
+            #if swift(>=3)
+                throw PGN.ParseError.noClosingQuote(self)
+            #else
+                throw PGN.ParseError.NoClosingQuote(self)
+            #endif
+        }
+        guard !inComment else {
+            #if swift(>=3)
+                throw PGN.ParseError.noClosingBrace(self)
+            #else
+                throw PGN.ParseError.NoClosingBrace(self)
+            #endif
+        }
+        return stripped
     }
 
 }
--- Tests/SageTests.swift
+++ Tests/SageTests.swift
@@ -389,6 +389,34 @@ class SageTests: XCTestCase {
         }
     }
 
+    func testPGNParsing() {
+        let immortalGame = String()
+            + "[Event \"London\"]\n"
+            + "[Site \"London\"]\n"
+            + "[Date \"1851.??.??\"]\n"
+            + "[EventDate \"?\"]\n"
+            + "[Round \"?\"]\n"
+            + "[Result \"1-0\"]\n"
+            + "[White \"Adolf Anderssen\"]\n"
+            + "[Black \"Kieseritzky\"]\n"
+            + "[ECO \"C33\"]\n"
+            + "[WhiteElo \"?\"]\n"
+            + "[BlackElo \"?\"]\n"
+            + "[PlyCount \"45\"]\n"
+            + "\n"
+            + "1.e4 e5 2.f4 exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5\n"
+            + "9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6\n"
+            + "16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 19. e5 Qxa1+ 20. Ke2 Na6 21.Nxg7+ Kd8\n"
+            + "22.Qf6+ Nxf6 23.Be7# 1-0\n"
+        do {
+            let pgn = try PGN(parse: immortalGame)
+            XCTAssertEqual(pgn.moves.count, 45)
+            XCTAssertEqual(pgn.outcome, Game.Outcome._win(._white))
+        } catch {
+            XCTFail(String(error))
+        }
+    }
+
 }
 
 extension Array {
