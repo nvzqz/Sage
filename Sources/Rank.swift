54f0b75ed276c810c9f494981df4f892b7bebac9
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-01-01 00:32:11 -0500

    Update copyright year to include 2017

--- Sources/Rank.swift
+++ Sources/Rank.swift
@@ -2,7 +2,7 @@
 //  Rank.swift
 //  Sage
 //
-//  Copyright 2016 Nikolai Vazquez
+//  Copyright 2017 Nikolai Vazquez
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
